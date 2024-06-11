0956cb3837799880725a7e19f40b3a56fbfd51f8
GABRIEL VARGAS <gabriel.vargas@frogpay.com.br>
2024-06-11 02:39:58 -0300

    estrutura inicial do projeto

---
+++ TesteMeta/src/main/java/com/meta/TesteMeta/TesteMetaApplication.java
@@ -0,0 +1,13 @@
+package com.meta.TesteMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMetaApplication.class, args);
+	}
+
+}
---
+++ TesteMeta/src/test/java/com/meta/TesteMeta/TesteMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.TesteMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
